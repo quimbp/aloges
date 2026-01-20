a0d3e23571c197324a5b654e55c2c1c080b692a1
Joaquim Ballabrera <joaquim@icm.csic.es>
2026-01-20 17:57:05 +0100

    Add examples

--- src/examples/example_color.f90
+++ src/examples/example_color.f90
@@ -2,7 +2,7 @@ use module_color ! or: use aloges
 implicit none (type, external)
 
 call print_colored('This text is in red', red)      ! red is a parameter constant
-call print_colored('This text is in blue', blue)    ! red is a parameter constant
+call print_colored('This text is in blue', blue)    ! blue is a parameter constant
 call print_colored('This text is in green', green)  ! green is a parameter constant
 
 write(*,'(A)') red // 'Red' // reset // ' and ' // blue // ' Blue ' // reset
